6da1e4fc24bdce7cb0888b522f05c714d138608e
Pawel Polewicz <p.polewicz@gmail.com>
2013-05-19 02:23:19 +0200

    Add queen.get_name()

--- antcolony/queen.py
+++ antcolony/queen.py
@@ -1,20 +1,14 @@
-class Queen(object):
-    pass
-
-class QueenInWorldPerformance(object):
-    def __init__(self, queen, world):
-        self.queen = queen
-        self.world = world
-
-class QueenCombinedPerformance(object):
-    pass
-
-class QueenEvaluator(object):
-    pass
-
-class BasicQueen(object):
-    def __init__(self, ant_class):
-        self.ant_class = ant_class
+class AbstractQueen(object):
     def spawn_ants(self, amount_of_ants):
-        return [self.ant_class] * amount_of_ants
+        raise NotImplementedError()
+    def get_name(self):
+        raise NotImplementedError()
+
+class BasicQueen(AbstractQueen):
+    def __init__(self, ant_class):
+        self._ant_class = ant_class
+    def spawn_ants(self, amount_of_ants):
+        return [self._ant_class] * amount_of_ants
+    def get_name(self):
+        return '%s-%s' % (self.__class__.__name__, self._ant_class.__name__)
 
