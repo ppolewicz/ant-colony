b86c53c388c39baee1ddfe3a615cdad20d272055
Pawel Polewicz <p.polewicz@gmail.com>
2014-03-27 14:15:24 +0100

    Make avg() work with iterators

--- antcolony/util.py
+++ antcolony/util.py
@@ -1,7 +1,12 @@
 import json
 
 def avg(iterable):
-    return sum(iterable) / len(iterable)
+    sum_ = 0
+    element_count = 0
+    for element in iterable:
+        sum_ += element
+        element_count += 1
+    return sum_ / element_count
 
 def nice_json_dump(data, filepath):
     with open(filepath, 'w') as f:
